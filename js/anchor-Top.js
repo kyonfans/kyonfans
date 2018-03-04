2e86135fe9ba2cf16c791ca3e124be10f279c59c
kyonfans <s9811154@gmail.com>
2018-03-04 16:55:21 +0800

    landing page

--- js/anchor-Top.js
+++ js/anchor-Top.js
@@ -5,3 +5,24 @@ $("a[href='#top']").click(function() {
   return false;
 });
 });
+//personal
+$(function(){
+$("a[href='#personal']").click(function() {
+  $("html, body").animate({scrollTop:$('#testimonial-block-wrap').offset().top}, "2000");
+  return false;
+});
+});
+//work
+$(function(){
+$("a[href='#work']").click(function() {
+  $("html, body").animate({scrollTop:$('#service-block-wrap').offset().top}, "2000");
+  return false;
+});
+});
+//history
+$(function(){
+$("a[href='#history']").click(function() {
+  $("html, body").animate({scrollTop:$('#history').offset().top}, "2000");
+  return false;
+});
+});
